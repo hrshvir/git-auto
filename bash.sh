1fff543011a8b8523094deb4b39b20ff26f02049
hrshvir <kalras48@gmail.com>
2024-03-26 22:57:46 -0400

    hello

--- bash.sh
+++ bash.sh
@@ -3,20 +3,9 @@
 # Build the Docker image
 docker build -t testwithgit .
 
-# Function to prompt user for commit message
-get_commit_message() {
-    read -p "Enter commit message: " message
-    echo "$message"
-}
+read -p "Enter commit message: " message
 
-# Git commands
-git_command_1="git add ."
-git_command_2="git commit -m"
-git_command_3="git push"
-
-# Execute git commands
-eval "$git_command_1"
-commit_message=$(get_commit_message)
-eval "$git_command_2 '$commit_message'"
-eval "$git_command_3"
+git add .
+git commit -m "$message"
+git push
 
